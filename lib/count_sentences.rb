3f88c7bb41c86b6c3e556c720877811fcc4d61a1
Jennifer <jenndestes@gmail.com>
2021-02-11 14:32:58 -0600

    commit tree ea35fa3c2cc24e6fb906b56ad5859749cfb0e33c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,57 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    if end_with?(".")
+        return true
+    else 
+        return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+        return true
+    else 
+        return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(" ").count do |word|
+      word.exclamation? || word.question? || word.sentence?
+    end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if self.end_with?(".", "?", "!")
+#   return self.split.count
+# else
+#   return self
+# end
